35d80ad3820e01fe4e4d48d2e1967e03262fe8a5
Leonardo Odilon <odilon@ibm.com>
2024-07-26 15:18:11 -0300

    python yml

---
+++ token_request.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+USERNAME=$1
+PASSWORD=$2
+CLIENT_ID=$3
+CLIENT_SECRET=$4
+TOKEN_URL=$5
+
+RESPONSE=$(curl -X POST "$TOKEN_URL" --silent \
+  -d "grant_type=password" \
+  -d "client_id=$CLIENT_ID" \
+  -d "client_secret=$CLIENT_SECRET" \
+  -d "username=$USERNAME" \
+  -d "password=$PASSWORD")
+
+ACCESS_TOKEN=$(echo $RESPONSE | jq -r '.access_token')
